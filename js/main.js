bab1f582c230b08a1611a8eab276749c0e6dddfc
arbaaz <mirzaarbaaz123@gmail.com>
2022-06-09 21:05:56 -0400

    input logo, thispersondoesnotexist API, and light/dark mode switch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-// fetch("https://wallhaven.cc/api/v1/w/94x38z")
-//     .then(res => res.json())
-//     .then(data => console.log(data))
+fetch("https://fakeface.rest/face/json")
+    .then(res => res.json())
+    .then(data => {
+        document.querySelector('.testimonyPic').src = data.image_url
+    })
+
+document.querySelector('.theme').addEventListener('click', switchTheme)
+    
+function switchTheme() {
+    let background = document.querySelector('body').style.background
+
+    if (background === "black") {
+        document.querySelector('body').style.background = "white"
+    } else {
+        document.querySelector('body').style.background = "black"
+    }
+}
